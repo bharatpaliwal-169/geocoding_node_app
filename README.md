0de0d2b80688b1f9c73806872bcbeb588f419263
Bharat Paliwal <18uec169@lnmiit.ac.in>
2022-09-24 00:18:27 +0530

    final push v1

---
+++ README.md
@@ -0,0 +1,21 @@
+# KrazeApp Assignment
+
+GeoCoding API
+
+To run this application in command line simple follow the following commands.
+
+```
+npm install
+```
+
+then
+
+```
+npm start input.txt
+```
+
+You can read the output in console. Also you can check output.txt
+
+You can try with multiple inputs, exceptions are handled and code is kept very straight-forward.
+
+This is build with the approach for command line application. Other approach could be simpley building an Frontend application or a simple standalone backend application.
--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import fs from 'fs';
 
 
 const apikey = '9b87458bc731fc48ac8f3d9695817850';
-
-// in-memory store
-const store = [];
+const baseURL = 'http://api.positionstack.com/v1/forward';
 
 //main
 try {
@@ -25,29 +23,18 @@ try {
   if(lines.length > 0 && lines[0] != '') {
 
     lines.map(loc => {
-      // const store = new Map();
         // NOTE : Provinding valid input is assumed. || Data filter
         if(loc !== undefined && loc !== null && loc !== '' && loc !== isNaN(loc)) {
           
-          // if(store.find(i => i.loc === loc)) {
-          //   console.log('store hit');
-          //   // write to output file
-          //   var res = store.find(i => i.loc === loc).content;
-          //   fs.appendFileSync(outputFile, res, (err) => {
-          //     if(err) console.log(err);
-          //   })
-          //   return;
-          // }
-
-          axios.get(`http://api.positionstack.com/v1/forward?access_key=${apikey}&query=${loc}&limit=1`) //limit is put to 1 for our use case
-          .then (response => {
+          axios.get(`${baseURL}?access_key=${apikey}&query=${loc}&limit=1`) //limit is put to 1 for our use case
+            .then (response => {
             // console.log(response.data);
             
             if(response.data.data[0] && response.data.data[0] != null && response.data.data[0] != undefined) {
               var content = loc + " -> " +response.data.data[0].latitude + "," + response.data.data[0].longitude+"\r\n";
               console.log(content);
               
-              // store.push({loc:content});
+
             }else{
               console.log("No such location found!");
             }
@@ -59,16 +46,16 @@ try {
             });
             
           }).catch (error => {
-            console.log('Error : API calling error ; invalid input ' + loc);
-            const err = error;
-          });
-        }
-      });
-
+          console.log('Error : API calling error ; invalid input ' + loc);
+          const err = error;
+        });
+      }
+    });
+    
   }else{
     console.log("File is empty");
   }
-
+  
 } catch (error) {
   console.log("ERROR : File name not given in console command");
   console.log("Use command : node start input.txt");
